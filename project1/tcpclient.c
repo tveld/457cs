69d8a9164de1b815dcf07928b58c401d489a84ef
Matthew Pairitz <pairitma@arch05.cis.gvsu.edu>
2017-02-03 16:01:34 -0500

    error checking no longer exits the program

--- project1/tcpclient.c
+++ project1/tcpclient.c
@@ -9,7 +9,8 @@
 #include <string.h>
 
 int main(int argc, char** argv){
-  int port; // port number to send to
+        int port=0; // port number to send to
+	int e = -1; // connection return value
 	char ip[5000]; // ip address to send to
 	char fname[5000]; // file name you want to fetch
 	char nname[5000]; // the new name of the file
@@ -24,54 +25,51 @@ int main(int argc, char** argv){
 		return 1;
 	}
 
-	
-
 	//IPV4 Address, maintains info 
 	//even if sockaddr is cast to something else
 	serveraddr.sin_family=AF_INET;	
+
+	while(port < 1024 || port  > 49000){
+	  // ask for port number
+	  printf("What port do you want to listen to? \n");
+	  char input[100];
+	  scanf("%s", &input);
+	  port = atoi(input);
 	
-	// Get port from user input
-	printf("What port do you want to send to?\n");
+	  // validate input
+	  if(port < 1024 || port  > 49000){
+	    printf("This is not a valid port number");
+	    printf(" Please pick a port in the range of 1024 to 4900.\n");	
+	  }
+	}
 	
-	int err = scanf("%d", &port);
-	if(err < 0){
-		printf("Error getting the port number\n");
-	}
-
-	// validate port number
-	if(port < 1024 || port > 49000){
-		printf("This is not a valid port.");
-		printf("  Please pick a port in the range of 1024 to 4900.\n");
-		return 1;
-	}
-
-	// get the ip from user input
-	printf("What ip do you want to send to?\n");
+	while(e<0){
+	  // get the ip from user input
+	  printf("What ip do you want to send to?\n");
 	
-	int error = scanf("%s", &ip);
+	  int error = scanf("%s", &ip);
 
-	if(error < 0){
-		printf("Error reading ip.\n");
-	}
+	  if(error < 0){
+	    printf("Error reading ip.\n");
+	  }
 
-	//sets what port this app is listening to (1024-49000)
-	serveraddr.sin_port=htons(port);  
+	  //sets what port this app is listening to (1024-49000)
+	  serveraddr.sin_port=htons(port);  
 
-	// 127.0.0.1 is a loopback address
-	// address of client
-	serveraddr.sin_addr.s_addr=inet_addr(ip);
+	  // 127.0.0.1 is a loopback address
+	  // address of client
+	  serveraddr.sin_addr.s_addr=inet_addr(ip);
 	
-	// takes socket and address and binds them together
-	// makes sure someone is listening at that address
-	int e = connect(sockfd,(struct sockaddr*)&serveraddr, sizeof(serveraddr));
-
-	if(e < 0){
-		printf("There was an error with connecting to server.\n");
-		return 1;
-	}
+	  // takes socket and address and binds them together
+	  // makes sure someone is listening at that address
+	  e = connect(sockfd,(struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	  if(e < 0){
+	    printf("There was an error with connecting to server.\n");
+	    printf("Resetting to IP input...\n");
+	   }
+        }
 
 	//send data
-
 	printf("Enter the filename you wish to recieve:  ");
 	scanf("%s",fname);
 
