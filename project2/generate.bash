eaf1a41ad1a73d81c556ca82dc097fb1323d306f
tveld <veldhuit@mail.gvsu.edu>
2017-03-01 18:18:35 -0500

    added generate file

---
+++ project2/generate.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in $(seq 10000)
+do
+	echo $i >> sequence.txt
+done
---
+++ project2/generate.c
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+
+int main() {
+
+int i; // number of 
+
+	for(i = 0; i < 10000; ++i){
+		
+	}
+}
+
