4303e87ef4efad1b13e0ed60311f71a4c774b586
Troy Veldhuizen <veldhuit@mail.gvsu.edu>
2017-04-01 15:17:42 -0400

    moved send arp reply to function

--- project3/router.cpp
+++ project3/router.cpp
@@ -46,6 +46,55 @@ unsigned short cksum(unsigned short *buf, int count) {
 }
 
 
+void send_arp_reply(struct arpheader arp, int packet_socket, char *buf){
+	
+	// setup responce packet
+
+	//arp
+	arp.op=(htons(2));
+	memcpy(arp.dha, arp.sha, 6);
+	memcpy(arp.sha, ifeth1addr, 6);
+
+	// protocal addrs
+	unsigned char tmpaddr[4];
+	memcpy(tmpaddr, arp.dpa, 4);
+	memcpy(arp.dpa, arp.spa, 4);
+	memcpy(arp.spa, tmpaddr, 4);
+
+	// ether
+
+	//memcpy(eth->ether_dhost, recvaddr.sll_addr, 6);
+
+	memcpy(eth->ether_dhost, eth->ether_shost, 6);
+
+	memcpy(eth->ether_shost, ifeth1addr, 6);
+	// add to buffer
+
+	printf("ether saddr: %02X:%02X:%02X:%02X:%02X:%02X\n",
+	eth->ether_shost[0], eth->ether_shost[1], eth->ether_shost[2],
+	eth->ether_shost[3], eth->ether_shost[4], eth->ether_shost[5]);
+
+
+	printf("ether daddr: %02X:%02X:%02X:%02X:%02X:%02X\n",
+	eth->ether_dhost[0], eth->ether_dhost[1], eth->ether_dhost[2],
+	eth->ether_dhost[3], eth->ether_dhost[4], eth->ether_dhost[5]);
+
+
+	char responce[42];
+	memcpy(responce, eth, 14);
+	memcpy(&responce[14], &arp, 28);
+
+	printf("Size of eth + arp is: %d\n", (int) sizeof(responce));
+
+	int b = send(packet_socket, responce, 42, 0);
+
+	printf("%d bytes sent back\n==========================\n", b);
+
+
+
+
+}
+
 int main(){  
 	int packet_socket;
 	unsigned char* ifeth1addr;
@@ -171,7 +220,6 @@ int main(){
 			int size = sizeof(eth->ether_dhost)+sizeof(eth->ether_shost)+sizeof(eth->ether_type);
 			printf("Size of Eth header: %d\n", size);	
 			*/
-		
 			struct arpheader arp;
 
 			memcpy(&arp, &buf[sizeof(struct ether_header)], sizeof(struct arpheader));
@@ -203,50 +251,21 @@ int main(){
 				ifeth1addr[4],
 				ifeth1addr[5]
 			);
-			// setup responce packet
-
-			//arp
-			arp.op=(htons(2));
-			memcpy(arp.dha, arp.sha, 6);
-			memcpy(arp.sha, ifeth1addr, 6);
-
-			// protocal addrs
-			unsigned char tmpaddr[4];
-			memcpy(tmpaddr, arp.dpa, 4);
-			memcpy(arp.dpa, arp.spa, 4);
-			memcpy(arp.spa, tmpaddr, 4);
-
-			// ether
-
-			//memcpy(eth->ether_dhost, recvaddr.sll_addr, 6);
-
-			memcpy(eth->ether_dhost, eth->ether_shost, 6);
-
-			memcpy(eth->ether_shost, ifeth1addr, 6);
-			// add to buffer
-
-			printf("ether saddr: %02X:%02X:%02X:%02X:%02X:%02X\n",
-			eth->ether_shost[0], eth->ether_shost[1], eth->ether_shost[2],
-			eth->ether_shost[3], eth->ether_shost[4], eth->ether_shost[5]);
-
-
-			printf("ether daddr: %02X:%02X:%02X:%02X:%02X:%02X\n",
-			eth->ether_dhost[0], eth->ether_dhost[1], eth->ether_dhost[2],
-			eth->ether_dhost[3], eth->ether_dhost[4], eth->ether_dhost[5]);
-
-
-			char responce[42];
-			memcpy(responce, eth, 14);
-			memcpy(&responce[14], &arp, 28);
-
-			printf("Size of eth + arp is: %d\n", (int) sizeof(responce));
-
-			int b = send(packet_socket, responce, 42, 0);
-
-			printf("%d bytes sent back\n==========================\n", b);
 			
+			// if arp reply
+			if(ntohs(arp.oper) == "2"){
+				printf("I've got an ARP reply\n");
+
+
+				// arp request
+			} else if(ntohs(arp.oper) == "1"){
+				printf("I've got an ARP request\n");
+
+			}
+			
+			// ICMP packet
 		} else {
-			printf("I've got an ICMP packet");
+			printf("I've got an ICMP packet\n");
 			
 			struct ether_header eth_1;
 			struct iphdr ip_1;
